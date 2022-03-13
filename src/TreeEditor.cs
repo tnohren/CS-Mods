aa8ebdc75390f5cbf84085c96bbfc8a0b60187cd
Trey Nohren <twnohren@ucdavis.edu>
2022-03-13 15:21:20 -0700

    Create TreeEditor.cs

---
+++ src/TreeEditor.cs
@@ -0,0 +1,10 @@
+using ICities;
+
+namespace TreeEditor
+{
+    public class TreeEditor : IUserMod
+    {
+        public string Name => "Remove It";
+        public string Description => "Remove what you want";
+    }
+}
